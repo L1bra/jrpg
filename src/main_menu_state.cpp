b1c973ea641f5d208acfa58274c2d10e1526f089
L1bra <L1bradv86@gmail.com>
2021-02-13 22:17:39 +1000

    menu almost done

--- src/main_menu_state.cpp
+++ src/main_menu_state.cpp
@@ -1,8 +1,20 @@
 #include "main_menu_state.h"
 
+
 MainMenuState::MainMenuState(const StateMachine& state)
 {
-    // constructor
+    m_MainMenuTexture = ResourceManager::loadTexture(Textures::MenuBackground, "src/res/background/menu_state.png");
+    m_MainMenuSprite.setTexture(*m_MainMenuTexture);
+
+    m_MenuCursorTexture = ResourceManager::loadTexture(Textures::MenuSettings, "src/res/background/menu_cursor.png");
+    m_MenuCursorSprite.setTexture(*m_MenuCursorTexture);
+    m_MenuCursorSprite.setTextureRect(sf::IntRect(0, 0, 51, 1));    // play button
+    m_MenuCursorSprite.setScale(6.f, 6.f);                          // scale to native
+    m_MenuCursorSprite.setPosition(810.f, 288.f);                   // play button coords
+
+    sf::Vector2f targetSize(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height);
+    m_MainMenuSprite.setScale(targetSize.x / m_MainMenuSprite.getLocalBounds().width, 
+                            targetSize.y / m_MainMenuSprite.getLocalBounds().height);
 }
 
 MainMenuState::~MainMenuState()
@@ -10,14 +22,74 @@ MainMenuState::~MainMenuState()
     // destructor
 }
 
-void MainMenuState::Update(float elapsedTime)
+
+    // play:     810 288
+    // settings: 654 468
+    // exit      834 648
+
+void MainMenuState::Input(Direction direction)
 {
-    // update main menu
+
+    switch(direction)
+    {
+        case Up:
+        {
+            if(m_MenuCursorSprite.getPosition().y == 648.f)
+            {
+                m_MenuCursorSprite.setPosition({654.f, 468.f});
+            }
+            else if(m_MenuCursorSprite.getPosition().y == 468.f)
+            {
+                m_MenuCursorSprite.setPosition({810.f, 288.f});
+            }
+        } break;
+        
+        case Down:
+        {
+            if(m_MenuCursorSprite.getPosition().y == 288.f)
+            {
+                m_MenuCursorSprite.setPosition({654.f, 468.f});
+            }
+            else if(m_MenuCursorSprite.getPosition().y == 468.f)
+            {
+                m_MenuCursorSprite.setPosition({834.f, 648.f});
+            }
+        } break;
+        
+        case Left:
+        {
+            //
+        } break;
+
+        case Right:
+        {
+            //
+        } break;
+    }
 }
 
-void MainMenuState::Render()
+
+void MainMenuState::Update(float elapsedTime)
 {
-    // render main menu
+    if(m_MenuCursorSprite.getPosition().y == 288.f)
+    {
+        m_MenuCursorSprite.setTextureRect(sf::IntRect(0, 0, 51, 1));
+    }
+    else if(m_MenuCursorSprite.getPosition().y == 468.f)
+    {
+        m_MenuCursorSprite.setTextureRect(sf::IntRect(0, 0, 102, 1));
+    }
+    else if(m_MenuCursorSprite.getPosition().y == 648.f)
+    {
+        m_MenuCursorSprite.setTextureRect(sf::IntRect(0, 0, 42, 1));
+    }
+}
+
+
+void MainMenuState::Render(sf::RenderWindow& window)
+{
+    window.draw(m_MainMenuSprite);
+    window.draw(m_MenuCursorSprite);
 }
 
 void MainMenuState::OnEnter()
--- src/main_menu_state.h
+++ src/main_menu_state.h
@@ -1,21 +1,27 @@
 #ifndef MAIN_MENU_STATE_H_
 #define MAIN_MENU_STATE_H_
 
-
 #include "state_machine.h"
+
 
 class MainMenuState : public State
 {
 private:
     // private menu methods
+    std::shared_ptr<sf::Texture> m_MainMenuTexture;
+    sf::Sprite m_MainMenuSprite;
+
+    std::shared_ptr<sf::Texture> m_MenuCursorTexture;
+    sf::Sprite m_MenuCursorSprite;
 public:
     // public methods
 
     MainMenuState(const StateMachine& state);
     ~MainMenuState();
 
+    void Input(Direction direction);
     void Update(float elapsedTime);
-    void Render();
+    void Render(sf::RenderWindow& window);
     void OnEnter();
     void OnExit();
 };
