6c7386097851fd0e6e36a137fa657cb955e6d4c1
L1bra <L1bradv86@gmail.com>
2021-02-17 10:40:20 +1000

    simple animation

--- src/entity.cpp
+++ src/entity.cpp
@@ -35,21 +35,25 @@ void Entity::update(float dt)
                     {
                         case sf::Keyboard::Right:
                         {
+                            m_Sprite.setTextureRect(sf::IntRect(0, 0, 24, 24));
                             m_Position.x += m_Speed * dt;
                         } break;
 
                         case sf::Keyboard::Left:
                         {
+                            m_Sprite.setTextureRect(sf::IntRect(24, 0, 24, 24));
                             m_Position.x -= m_Speed * dt;
                         } break;
 
                         case sf::Keyboard::Up:
                         {
+                            m_Sprite.setTextureRect(sf::IntRect(0, 21, 24, 21));
                             m_Position.y -= m_Speed * dt;
                         } break;
 
                         case sf::Keyboard::Down:
                         {
+                            m_Sprite.setTextureRect(sf::IntRect(24, 21, 24, 21));
                             m_Position.y += m_Speed * dt;
                         } break;
                     }
@@ -62,6 +66,7 @@ void Entity::update(float dt)
         case Entity_state::Dead: {} break;  // TODO
     }
 
+    m_Sprite.setScale(32 / m_Sprite.getLocalBounds().width, 32 / m_Sprite.getLocalBounds().height);
     m_Sprite.setPosition(m_Position);
 }
 
@@ -85,6 +90,7 @@ Entity init_entity(sf::Vector2f position, sf::String sprite_path)
     entity.m_Position = position;
     entity.m_Texture_ptr = ResourceManager::loadTexture(Textures::Magic, sprite_path);
     entity.m_Sprite.setTexture(*entity.m_Texture_ptr);
+    entity.m_Sprite.setTextureRect(sf::IntRect(0, 0, 24, 24));
     entity.m_Sprite.setScale(scaleTarget.x / entity.m_Sprite.getLocalBounds().width,
                         scaleTarget.y / entity.m_Sprite.getLocalBounds().height);
 
